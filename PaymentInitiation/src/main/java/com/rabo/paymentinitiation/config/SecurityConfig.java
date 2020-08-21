ffff4f88c555fc6f824d17e4f0b68dcd4123dcdd
kguttala <Kalaiselvan_Guttalapandian@PremierInc.com>
2020-08-21 20:00:13 +0530

    Added swagger path in spring security

--- PaymentInitiation/src/main/java/com/rabo/paymentinitiation/config/SecurityConfig.java
+++ PaymentInitiation/src/main/java/com/rabo/paymentinitiation/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/","/swagger-ui.html").permitAll()
                 .and().x509().subjectPrincipalRegex("CN=(.*?),")
                 .userDetailsService(userDetailsService());
     }
