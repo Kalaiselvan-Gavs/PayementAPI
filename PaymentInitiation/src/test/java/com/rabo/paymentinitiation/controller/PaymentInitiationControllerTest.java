0d25f07f0a9df3090b085e99e2b7bb3e3a45fe74
kalaiselvan.sg@gavstech.com <kalaiselvan.sg@gavstech.com>
2020-09-06 06:14:36 +0530

    Payment request validation changes

--- PaymentInitiation/src/test/java/com/rabo/paymentinitiation/controller/PaymentInitiationControllerTest.java
+++ PaymentInitiation/src/test/java/com/rabo/paymentinitiation/controller/PaymentInitiationControllerTest.java
@@ -3,97 +3,164 @@ package com.rabo.paymentinitiation.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabo.paymentinitiation.filter.CustomFilter;
 import com.rabo.paymentinitiation.model.PaymentInitiationRequest;
 import com.rabo.paymentinitiation.service.PaymentService;
-import com.rabo.paymentinitiation.service.PaymentServiceImpl;
 import com.rabo.paymentinitiation.util.Constants;
-
-//import com.in28minutes.springboot.model.Course;
-//import com.in28minutes.springboot.service.StudentService;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.reset;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = PaymentInitiationController.class)
-//@WithMockUser
+@WebMvcTest(value = PaymentInitiationController.class, excludeAutoConfiguration = CustomFilter.class)
 public class PaymentInitiationControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@MockBean
-	private PaymentService paymentService;
+    @MockBean
+    private PaymentService service;
+    
+    private ObjectMapper objectMappaer;
+    
+    PaymentInitiationRequest paymentRequest;
 
-	private ObjectMapper objectMappaer;
-    
-    private PaymentInitiationRequest paymentRequest;
-   
-    
     @Before
-    public void init() {
-    	this.mockMvc = MockMvcBuilders.standaloneSetup(new PaymentInitiationController()).build();
+    public void setUp() throws Exception {
     	objectMappaer = new ObjectMapper();
     	paymentRequest = new PaymentInitiationRequest();
     	paymentRequest.setDebtorIBAN("NL02RABO7134384551");
         paymentRequest.setCreditorIBAN("NL94ABNA1008270121");
         paymentRequest.setAmount("1.0");
         paymentRequest.setEndToEndId("1");
-        
+    }
+
+    @Test
+    public void whenPaymentInitiationSuccess() throws Exception {
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(false);
+
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
+        		.header(Constants.X_REQUEST_ID, "1")
+        		.header(Constants.SIGNATURE_CERTIFICATE, "2")
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().isCreated());
+        reset(service);
     }
     
-	@Test
-	public void createPaymentTest() throws Exception {
+    @Test
+    public void whenCheckForAmoutLimitExceeded() throws Exception {
+    	paymentRequest.setDebtorIBAN("NL02RABO0022222222");
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(true);
 
-		Mockito.when(paymentService.checkForAmoutLimitExceeded(Mockito.anyObject())).thenReturn(false);
-		
-    	RequestBuilder requestBuilder = MockMvcRequestBuilders
- 				.post("/payment/v1.0.0/initiate-payment")
- 				.accept(MediaType.APPLICATION_JSON).content(objectMappaer.writeValueAsString(paymentRequest))
- 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
         		.header(Constants.X_REQUEST_ID, "1")
         		.header(Constants.SIGNATURE_CERTIFICATE, "2")
-				.header(Constants.SIGNATURE, "3");
-     
-	 	MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-	
-		MockHttpServletResponse response = result.getResponse();
-	
-	}
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().isUnprocessableEntity());
+        reset(service);
+    }
+    
+    @Test
+    public void whenCheckForEmptyDebtorIBAN() throws Exception {
+    	paymentRequest.setDebtorIBAN("");
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(true);
 
-	@Test
-	public void createPaymentFailTest() throws Exception {
-
-		Mockito.when(paymentService.checkForAmoutLimitExceeded(Mockito.anyObject())).thenReturn(false);
-		
-    	RequestBuilder requestBuilder = MockMvcRequestBuilders
- 				.post("/payment/v1.0.0/initiate-payment")
- 				.accept(MediaType.APPLICATION_JSON).content(objectMappaer.writeValueAsString(paymentRequest))
- 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
         		.header(Constants.X_REQUEST_ID, "1")
         		.header(Constants.SIGNATURE_CERTIFICATE, "2")
-				.header(Constants.SIGNATURE, "3");
-     
-	 	MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-	
-		MockHttpServletResponse response = result.getResponse();
-	
-	}
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().is4xxClientError());
+        reset(service);
+    }
+    
+    @Test
+    public void whenCheckForInvalidDebtorIBAN() throws Exception {
+    	paymentRequest.setDebtorIBAN("NL02RAB");
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(true);
 
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
+        		.header(Constants.X_REQUEST_ID, "1")
+        		.header(Constants.SIGNATURE_CERTIFICATE, "2")
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().is4xxClientError());
+        reset(service);
+    }
+    
+    @Test
+    public void whenCheckForEmptyCreditorIBAN() throws Exception {
+    	paymentRequest.setCreditorIBAN("");
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(true);
+
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
+        		.header(Constants.X_REQUEST_ID, "1")
+        		.header(Constants.SIGNATURE_CERTIFICATE, "2")
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().is4xxClientError());
+        reset(service);
+    }
+    
+    @Test
+    public void whenCheckForInvalidCreditorIBAN() throws Exception {
+    	paymentRequest.setCreditorIBAN("NL94ABN");
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(true);
+
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
+        		.header(Constants.X_REQUEST_ID, "1")
+        		.header(Constants.SIGNATURE_CERTIFICATE, "2")
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().is4xxClientError());
+        reset(service);
+    }
+    
+    @Test
+    public void whenCheckForAmountEmpty() throws Exception {
+    	paymentRequest.setAmount("");
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(true);
+
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
+        		.header(Constants.X_REQUEST_ID, "1")
+        		.header(Constants.SIGNATURE_CERTIFICATE, "2")
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().is4xxClientError());
+        reset(service);
+    }
+    
+    @Test
+    public void whenCheckForInvalidAmount() throws Exception {
+    	paymentRequest.setAmount("a.00");
+        given(service.checkForAmoutLimitExceeded(Mockito.any())).willReturn(true);
+
+        mvc.perform(post("/payment/v1.0.0/initiate-payment")
+        		.contentType(MediaType.APPLICATION_JSON)
+        		.content(objectMappaer.writeValueAsString(paymentRequest))
+        		.header(Constants.X_REQUEST_ID, "1")
+        		.header(Constants.SIGNATURE_CERTIFICATE, "2")
+        		.header(Constants.SIGNATURE, "3"))
+        		.andExpect(status().is4xxClientError());
+        reset(service);
+    }
+    
 }
-
